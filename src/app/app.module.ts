7c945605aa2b31cb034d211821804dea35448192
Yaroslav <ufamoonguard@gmail.com>
2017-10-12 01:58:48 +0500

    refactoring import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { UsersService } from './shared/services/users.service';
-import { HttpModule } from '@angular/http';
 import { AuthService } from './shared/services/auth.service';
 
 @NgModule({
