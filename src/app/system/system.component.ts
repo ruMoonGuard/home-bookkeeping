25e4fa8e9f16034e604550c74a9422ceac3a7046
Yaroslav <ufamoonguard@gmail.com>
2017-10-12 15:47:27 +0500

    add empty template system module

---
+++ src/app/system/system.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'csw-system',
+  templateUrl: './system.component.html'
+})
+export class SystemComponent {}
---
+++ src/app/system/system.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from '../shared/shared.module';
+import { SystemComponent } from './system.component';
+import { BillPageComponent } from './bill-page/bill-page.component';
+import { HistoryPageComponent } from './history-page/history-page.component';
+import { PlanningPageComponent } from './planning-page/planning-page.component';
+import { RecordsPageComponent } from './records-page/records-page.component';
+import { SystemRoutingModule } from './system-routing.module';
+
+@NgModule({
+  declarations: [
+    SystemComponent,
+    BillPageComponent,
+    HistoryPageComponent,
+    PlanningPageComponent,
+    RecordsPageComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    SystemRoutingModule
+  ]
+})
+export class SystemModule {}
