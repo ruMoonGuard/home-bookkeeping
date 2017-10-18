0dc98a48cb6b86a4bc54f771d81e1071c6a7ed3f
Yaroslav <ufamoonguard@gmail.com>
2017-10-19 01:47:54 +0500

    Added history events page

--- src/app/system/history-page/history-events/history-events.component.ts
+++ src/app/system/history-page/history-events/history-events.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Category } from '../../../models/category.model';
+import { EventChangeMoney } from '../../../models/event.model';
 
 @Component({
   selector: 'csw-history-events',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoryEventsComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+  @Input() events: EventChangeMoney[] = [];
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  getCategoryName(e: EventChangeMoney): string {
+    return this.categories.find(c => c.id === e.category).name;
+  }
 }
