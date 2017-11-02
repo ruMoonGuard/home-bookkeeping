ff1cc9cd285043f30a2c0ee4d2a28573b5cbbc9e
Yaroslav <ufamoonguard@gmail.com>
2017-11-03 00:59:03 +0500

    Added token controller

---
+++ API/HomeBookkeeping/Controllers/TokenController.cs
@@ -0,0 +1,82 @@
+﻿using HomeBookkeeping.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HomeBookkeeping.Controllers
+{
+    public class TokenController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public TokenController(UserManager<User> userManager) => _userManager = userManager;
+
+        [HttpPost, Route("create")]
+        public async Task Create(string username, string password)
+        {
+            var identity = GetIdentity(username, password).Result;
+
+            if (identity == null)
+            {
+                //to do error
+            }
+
+            var nowTime = DateTime.UtcNow;
+
+            var token = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                notBefore: nowTime,
+                claims: identity.Claims,
+                expires: nowTime.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(),
+                    SecurityAlgorithms.HmacSha256)
+            );
+
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            Response.ContentType = "application/json";
+            await Response.WriteAsync(JsonConvert.SerializeObject(response,
+                new JsonSerializerSettings { Formatting = Formatting.Indented }));
+        }
+
+        private async Task<ClaimsIdentity> GetIdentity(string username, string password)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+
+            var users = _userManager.Users.ToList();
+
+            if (user == null || !await _userManager.CheckPasswordAsync(user, password)) return null;
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Nbf,
+                    new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                new Claim(JwtRegisteredClaimNames.Exp,
+                    new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString())
+            };
+
+            var claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+
+            return claimsIdentity;
+        }
+    }
+}
---
+++ API/HomeBookkeeping/Models/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace HomeBookkeeping.Models
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer"; // издатель токена
+        public const string AUDIENCE = "http://localhost:51884/"; // потребитель токена
+        const string KEY = "mysupersecret_secretkey!123qwerty";   // ключ для шифрации
+        public const int LIFETIME = 100; // время жизни токена - 100 минут
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
